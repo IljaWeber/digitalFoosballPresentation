fe823f459747a08038f34a0b2700ddab663d4c05
m.huber <maximilian.huber@valtech.com>
2019-11-20 14:04:22 +0100

    Initial Commit

---
+++ backend/src/it/java/com/valtech/digitalFoosball/application/configuration/DigitalFoosballTestConfiguration.java
@@ -0,0 +1,7 @@
+package com.valtech.digitalFoosball.application.configuration;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class DigitalFoosballTestConfiguration {
+}
