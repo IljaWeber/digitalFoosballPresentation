3a91905ff2eaf30e5c0900e46e6b243456cb0088
m.huber <maximilian.huber@valtech.com>
2020-01-14 11:02:18 +0100

    changed props for production

--- frontend/src/main/resources/properties.js
+++ frontend/src/main/resources/properties.js
@@ -1,10 +1,10 @@
-// export const properties = {
-//     host: "https://digital-football.valtech.io/",
-//     port: 8888,
-//     context: "/digitalfoosball/",
-//     url: "https://digital-football.valtech.io/digitalfoosball/",
-//     sock_url: "ws://digital-football.valtech.io/digitalfoosball/"
-// };
+export const properties = {
+    host: "https://digital-football.valtech.io/",
+    port: 8888,
+    context: "/digitalfoosball/",
+    url: "https://digital-football.valtech.io/digitalfoosball/",
+    sock_url: "ws://digital-football.valtech.io/digitalfoosball/"
+};
 
 // export const properties = {
 //     host: "http://192.168.123.159",
@@ -14,10 +14,10 @@
 //     sock_url: "ws://192.168.123.159:8888/digitalfoosball/"
 // };
 
-export const properties = {
-    host: "localhost",
-    port: 8888,
-    context: "/digitalfoosball/",
-    url: "http://localhost:8888/digitalfoosball/",
-    sock_url: "ws://localhost:8888/digitalfoosball/"
-};
+// export const properties = {
+//     host: "localhost",
+//     port: 8888,
+//     context: "/digitalfoosball/",
+//     url: "http://localhost:8888/digitalfoosball/",
+//     sock_url: "ws://localhost:8888/digitalfoosball/"
+// };
