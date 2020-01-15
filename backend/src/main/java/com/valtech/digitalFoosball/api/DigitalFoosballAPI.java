75492b0b7bef3a281c2beaf12b8950e2a8bf8e11
m.huber <maximilian.huber@valtech.com>
2020-01-15 13:59:55 +0100

    reset the changes for testing purpose

--- backend/src/main/java/com/valtech/digitalFoosball/api/DigitalFoosballAPI.java
+++ backend/src/main/java/com/valtech/digitalFoosball/api/DigitalFoosballAPI.java
@@ -41,10 +41,8 @@ public class DigitalFoosballAPI {
         return gameManager.getGameData();
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(path = "/allTeams", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<TeamOutput> getAllTeamsStartingWith() {
-        logger.info("All Teams requested");
         return gameManager.getAllTeams();
     }
 
