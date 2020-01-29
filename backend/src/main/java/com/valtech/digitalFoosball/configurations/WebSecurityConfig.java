21bd5fec3bd7870117d84c3da8011444e3038238
m.huber <maximilian.huber@valtech.com>
2020-01-29 15:20:18 +0100

    disabled x-frame-options

--- backend/src/main/java/com/valtech/digitalFoosball/configurations/WebSecurityConfig.java
+++ backend/src/main/java/com/valtech/digitalFoosball/configurations/WebSecurityConfig.java
@@ -9,7 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().sameOrigin()
+        http.headers().frameOptions().disable()
                 .and().csrf().disable().authorizeRequests()
                 .antMatchers("/", "api").permitAll();
     }
