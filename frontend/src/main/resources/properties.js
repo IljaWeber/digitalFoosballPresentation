544fa8711408918f2f98fb96009b600b297af9ea
m.huber <maximilian.huber@valtech.com>
2020-01-29 13:42:06 +0100

    changed allowed origins in websocket

--- frontend/src/main/resources/properties.js
+++ frontend/src/main/resources/properties.js
@@ -1,7 +1,6 @@
 export const properties = {
     url: "https://digital-football.valtech.io/api/",
     hostAndPort: 'https://digital-football.valtech.io/',
-    sock_url: "ws://digital-football.valtech.io/api/",
     auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
 };
 
