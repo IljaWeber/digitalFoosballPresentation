56102ff92ae8237bcfe13a3cd795d1bcb0c55d8f
m.huber <maximilian.huber@valtech.com>
2020-01-29 15:29:42 +0100

    x-frame-options: same origin

--- backend/src/main/java/com/valtech/digitalFoosball/configurations/WebSecurityConfig.java
+++ backend/src/main/java/com/valtech/digitalFoosball/configurations/WebSecurityConfig.java
@@ -9,7 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().disable()
+        http.headers().frameOptions().sameOrigin()
                 .and().csrf().disable().authorizeRequests()
                 .antMatchers("/", "api").permitAll();
     }
