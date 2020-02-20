eca60ee923564704bf87a92c68984b1466b4fc0d
m.huber <maximilian.huber@valtech.com>
2020-02-20 14:56:09 +0100

    added cors config

--- frontend/src/main/resources/properties.js
+++ frontend/src/main/resources/properties.js
@@ -4,10 +4,12 @@ export const properties = {
     auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
 };
 
-// export const properties = {
-//     url: "http://localhost:8888/api/",
-//     hostAndPort: 'http://localhost:8888/',
-//     sock_url: "ws://localhost:8888/",
-//     auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
-// };
+/*
+export const properties = {
+    url: "http://localhost:8888/api/",
+    hostAndPort: 'http://localhost:8888/',
+    sock_url: "ws://localhost:8888/",
+    auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
+};
+*/
 
