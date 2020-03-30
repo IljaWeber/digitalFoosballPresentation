d086a1b5cf4a27755a4735930d45b5a88b5b570e
Ilja Weber <ilja.weber@valtech.com>
2020-03-30 10:45:35 +0200

    removing unnecessary fields

--- backend/src/test/java/com/valtech/digitalFoosball/service/GameManagerShouldUndoLastGoal.java
+++ backend/src/test/java/com/valtech/digitalFoosball/service/GameManagerShouldUndoLastGoal.java
@@ -13,38 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameManagerShouldUndoLastGoal extends GameManagerTest {
 
-    private TeamOutput teamTwo;
-    private TeamOutput teamOne;
-    private List<TeamOutput> teams;
-    private GameDataModel expected;
-
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp();
-        teams = new ArrayList<>();
-        teamOne = new TeamOutput();
-        teamTwo = new TeamOutput();
-        expected = new GameDataModel();
-
-        teamOne.setName("T1");
-        teamOne.setPlayerOne("P1");
-        teamOne.setPlayerTwo("P2");
-
-        teamTwo.setName("T2");
-        teamTwo.setPlayerOne("P3");
-        teamTwo.setPlayerTwo("P4");
-
-        teams.add(teamOne);
-        teams.add(teamTwo);
-
-        expected.setTeams(teams);
     }
 
     @Test
     void is_possible_if_a_goal_was_shot() {
         gameManager.initGame(initDataModel);
-        setExpectedScore(3, 1);
         raiseActual(1, 2, 1, 1, 1);
 
         gameManager.undoLastGoal();
@@ -53,10 +30,6 @@ public class GameManagerShouldUndoLastGoal extends GameManagerTest {
                 "T1", "P1", "P2", 3, "T2", "P3", "P4", 1, 0, 0);
     }
 
-    private void setExpectedScore(int scoreOfTeamOne, int scoreOfTeamTwo) {
-        teams.get(0).setScore(scoreOfTeamOne);
-        teams.get(1).setScore(scoreOfTeamTwo);
-    }
 
     private void raiseActual(int... teams) {
 
