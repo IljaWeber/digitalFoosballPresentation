4ee3b0a89868e6be1133e0f3ff77618180cec21e
m.huber <maximilian.huber@valtech.com>
2020-04-08 10:02:10 +0200

    refactoring

--- backend/src/test/java/com/valtech/digitalFoosball/model/internal/PlayerDataModelShould.java
+++ backend/src/test/java/com/valtech/digitalFoosball/model/internal/PlayerDataModelShould.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PlayerDataModelTest {
+public class PlayerDataModelShould {
     @Test
-    public void resetValues_whenResetValuesIsCalled_thenSetEmptyName() {
+    public void have_an_empty_string_as_name_after_reset() {
         PlayerDataModel playerDataModel = new PlayerDataModel();
         playerDataModel.setName("P1");
 
