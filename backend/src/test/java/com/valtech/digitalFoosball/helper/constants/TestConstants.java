acce8d470aecaace0f242fa1f65383d005e6e1a9
m.huber <maximilian.huber@valtech.com>
2020-04-08 12:11:51 +0200

    made constants final

--- backend/src/test/java/com/valtech/digitalFoosball/helper/constants/TestConstants.java
+++ backend/src/test/java/com/valtech/digitalFoosball/helper/constants/TestConstants.java
@@ -2,7 +2,7 @@ package com.valtech.digitalFoosball.helper.constants;
 
 public class TestConstants {
 
-    public static int NO_WINNER = 0;
-    public static int TEAM_ONE = 1;
-    public static int TEAM_TWO = 2;
+    public final static int NO_WINNER = 0;
+    public final static int TEAM_ONE = 1;
+    public final static int TEAM_TWO = 2;
 }
