8f6114789c98ae4fd3141c9d4837ce762574889c
Ilja Weber <ilja.weber@valtech.com>
2020-04-28 10:54:22 +0200

    renaming

---
+++ backend/src/test/java/com/valtech/digitalFoosball/service/manager/TimeManagerShould.java
@@ -0,0 +1,46 @@
+package com.valtech.digitalFoosball.service.manager;
+
+import com.valtech.digitalFoosball.constants.Team;
+import com.valtech.digitalFoosball.model.GameDataModel;
+import com.valtech.digitalFoosball.model.internal.TeamDataModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TimeManagerShould {
+
+    private TimeManager timeManager;
+    private TeamDataModel teamDataModelOne;
+    private TeamDataModel teamDataModelTwo;
+    private GameDataModel gameDataModel;
+
+    @BeforeEach
+    void setUp() {
+        timeManager = new TimeManager();
+        teamDataModelOne = new TeamDataModel("T1", "P1", "P2");
+        teamDataModelTwo = new TeamDataModel("T2", "P3", "P4");
+
+        List<TeamDataModel> teams;
+        teams = new ArrayList<>();
+        teams.add(teamDataModelOne);
+        teams.add(teamDataModelTwo);
+
+        gameDataModel = new GameDataModel(teams);
+    }
+
+    @Test
+    void changeover() {
+
+        assertThat(teamDataModelOne.getScore()).isEqualTo(1);
+    }
+
+    private void countGoalForTeam(Team... teams) {
+        for (Team team : teams) {
+            timeManager.countGoalFor(team, gameDataModel);
+        }
+    }
+}
