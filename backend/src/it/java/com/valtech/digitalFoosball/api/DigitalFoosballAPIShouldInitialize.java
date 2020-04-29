fb10406f054c69df1520fda9baa4357cc58b1909
Ilja Weber <ilja.weber@valtech.com>
2020-04-29 15:23:48 +0200

    added new rest endpoint for initialisation of different game modes

--- backend/src/it/java/com/valtech/digitalFoosball/api/DigitalFoosballAPIShouldInitialize.java
+++ backend/src/it/java/com/valtech/digitalFoosball/api/DigitalFoosballAPIShouldInitialize.java
@@ -8,7 +8,6 @@ import com.valtech.digitalFoosball.model.input.InitDataModel;
 import com.valtech.digitalFoosball.model.internal.TeamDataModel;
 import com.valtech.digitalFoosball.model.output.GameOutputModel;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +19,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,48 +35,77 @@ public class DigitalFoosballAPIShouldInitialize {
     @Autowired
     private MockMvc mockMvc;
 
-    private ObjectMapper mapper;
-    private String expectedResponseBody;
-    private MvcResult result;
-    private MockHttpServletRequestBuilder builder;
     private Gson gson;
+    private String json;
+    private MvcResult result;
+    private ObjectMapper mapper;
+    private List<TeamDataModel> teams;
+    private GameDataModel gameDataModel;
     private InitDataModel initDataModel;
+    private String expectedResponseBody;
+    private GameOutputModel expectedValues;
+    private MockHttpServletRequestBuilder builder;
 
-    @BeforeEach
-    void setUp() throws Exception {
+    public DigitalFoosballAPIShouldInitialize() {
         gson = new Gson();
+        teams = new ArrayList<>();
         mapper = new ObjectMapper();
-        GameDataModel gameDataModel = new GameDataModel();
-        TeamDataModel teamOne = new TeamDataModel("Orange", "Goalie", "Striker");
-        TeamDataModel teamTwo = new TeamDataModel("Green", "Goalie", "Striker");
-        List<TeamDataModel> teams = new ArrayList<>();
-        teams.add(teamOne);
-        teams.add(teamTwo);
-
+        gameDataModel = new GameDataModel();
         initDataModel = new InitDataModel();
-        initDataModel.setTeams(teams);
-
-        gameDataModel.setTeam(ONE, teamOne);
-        gameDataModel.setTeam(TWO, teamTwo);
-        gameDataModel.setSetWinner(NO_TEAM);
-        GameOutputModel expectedValues = new GameOutputModel(gameDataModel);
-        expectedValues.setMatchWinner(Team.NO_TEAM);
-
-        expectedResponseBody = mapper.writeValueAsString(expectedValues);
-
     }
 
     @Test
     void an_ad_hoc_match_with_default_values_for_the_teams() throws Exception {
-        String json = gson.toJson(initDataModel);
-        builder = MockMvcRequestBuilders.post("/api/initialize/{gameModeId}", 0);
-        builder.contentType(MediaType.APPLICATION_JSON_VALUE).content(json);
+        prepareTeamsForInitialization(new TeamDataModel("Orange", "Goalie", "Striker"),
+                                      new TeamDataModel("Green", "Goalie", "Striker"));
 
-        result = mockMvc.perform(builder)
-                        .andExpect(status().isOk())
-                        .andReturn();
+        result = performRequestForGameModeInitialisation(0);
 
         String actualResponseBody = result.getResponse().getContentAsString();
         assertThat(actualResponseBody).isEqualTo(expectedResponseBody);
     }
+
+    @Test
+    void a_ranked_game_with_individual_team_and_player_names() throws Exception {
+        prepareTeamsForInitialization(new TeamDataModel("T1", "P1", "P2"), new TeamDataModel("T2", "P3", "P4"));
+
+        result = performRequestForGameModeInitialisation(1);
+
+        String actualResponseBody = result.getResponse().getContentAsString();
+        assertThat(actualResponseBody).isEqualTo(expectedResponseBody);
+    }
+
+    private MvcResult performRequestForGameModeInitialisation(int gameMode) throws Exception {
+
+        builder = MockMvcRequestBuilders.post("/api/initialize/{gameModeId}", gameMode);
+        builder.contentType(MediaType.APPLICATION_JSON_VALUE).content(json);
+
+        return mockMvc.perform(builder)
+                      .andExpect(status().isOk())
+                      .andReturn();
+    }
+
+    private void prepareTeamsForInitialization(TeamDataModel teamOne, TeamDataModel teamTwo) throws IOException {
+        this.initDataModel = new InitDataModel();
+        this.teams = new ArrayList<>();
+        teams.add(teamOne);
+        teams.add(teamTwo);
+
+        initDataModel.setTeams(teams);
+        json = gson.toJson(initDataModel);
+
+        prepareExpectedResponse(teamOne, teamTwo);
+    }
+
+    private void prepareExpectedResponse(TeamDataModel teamOne, TeamDataModel teamTwo) throws IOException {
+        this.gameDataModel.setTeam(ONE, teamOne);
+        this.gameDataModel.setTeam(TWO, teamTwo);
+        this.gameDataModel.setSetWinner(NO_TEAM);
+
+        expectedValues = new GameOutputModel(this.gameDataModel);
+        expectedValues.setMatchWinner(Team.NO_TEAM);
+
+        expectedResponseBody = mapper.writeValueAsString(expectedValues);
+    }
+
 }
