c8074187a4cacbe1b27f7cff28a20fb14016c745
m.huber <maximilian.huber@valtech.com>
2020-07-28 18:37:31 +0200

    implemented timerTask

---
+++ backend/src/main/java/com/valtech/digitalFoosball/domain/timeGame/HalftimeTimerTask.java
@@ -0,0 +1,17 @@
+package com.valtech.digitalFoosball.domain.timeGame;
+
+import java.util.TimerTask;
+
+public class HalftimeTimerTask extends TimerTask {
+
+    private final TimeGameRules timeGame;
+
+    public HalftimeTimerTask(TimeGameRules timeGame) {
+        this.timeGame = timeGame;
+    }
+
+    @Override
+    public void run() {
+        timeGame.startNextGameSequence();
+    }
+}
---
+++ backend/src/test/java/com/valtech/digitalFoosball/domain/timeGame/HalftimeTimerTaskShould.java
@@ -0,0 +1,28 @@
+package com.valtech.digitalFoosball.domain.timeGame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HalftimeTimerTaskShould {
+
+    @Test
+    void inform_the_game_when_the_time_is_over() {
+        TimeGameRulesFake timeGame = new TimeGameRulesFake();
+        HalftimeTimerTask task = new HalftimeTimerTask(timeGame);
+
+        task.run();
+
+        boolean actual = timeGame.informed;
+        assertThat(actual).isTrue();
+    }
+
+    private class TimeGameRulesFake extends TimeGameRules {
+        boolean informed = false;
+
+        @Override
+        public void startNextGameSequence() {
+            informed = true;
+        }
+    }
+}
