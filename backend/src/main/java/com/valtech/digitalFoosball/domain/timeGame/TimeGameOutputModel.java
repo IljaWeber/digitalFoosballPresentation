226607942cb7edaf1b37e7a5b120ee47b076051c
m.huber <maximilian.huber@valtech.com>
2020-07-30 14:54:51 +0200

    added gameState in TimeGameOutputModel

--- backend/src/main/java/com/valtech/digitalFoosball/domain/timeGame/TimeGameOutputModel.java
+++ backend/src/main/java/com/valtech/digitalFoosball/domain/timeGame/TimeGameOutputModel.java
@@ -4,19 +4,20 @@ import com.valtech.digitalFoosball.domain.common.constants.Team;
 import com.valtech.digitalFoosball.domain.common.converter.Converter;
 import com.valtech.digitalFoosball.domain.common.models.GameDataModel;
 import com.valtech.digitalFoosball.domain.common.models.output.game.BaseGameOutputModel;
-import com.valtech.digitalFoosball.domain.common.models.output.game.GameOutputModel;
 
-public class TimeGameOutputModel extends BaseGameOutputModel implements GameOutputModel {
+public class TimeGameOutputModel extends BaseGameOutputModel {
+    private final GameState gameState;
+
     public TimeGameOutputModel(GameDataModel model, TimeGameRules timeGameRules) {
         super.teams = Converter.convertMapToTeamOutputs(model.getTeams());
         super.matchWinner = timeGameRules.determineWinner();
+        gameState = timeGameRules.getGameState();
 
         teams.get(0).setScore(timeGameRules.getScoreOfTeam(Team.ONE));
         teams.get(1).setScore(timeGameRules.getScoreOfTeam(Team.TWO));
     }
 
-    @Override
-    public Team getWinnerOfSet() {
-        return null;
+    public GameState getGameState() {
+        return gameState;
     }
 }
