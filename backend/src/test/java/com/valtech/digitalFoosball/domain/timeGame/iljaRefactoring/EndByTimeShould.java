f5ea083e30209d86e258364ab3d4f7399d3c6e53
Ilja Weber <ilja.weber@valtech.com>
2020-08-03 09:50:51 +0200

    there is no winner when time is over and teams has an equal score

--- backend/src/test/java/com/valtech/digitalFoosball/domain/timeGame/iljaRefactoring/EndByTimeShould.java
+++ backend/src/test/java/com/valtech/digitalFoosball/domain/timeGame/iljaRefactoring/EndByTimeShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Stack;
 
-import static com.valtech.digitalFoosball.domain.common.constants.Team.ONE;
+import static com.valtech.digitalFoosball.domain.common.constants.Team.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class EndByTimeShould {
@@ -21,4 +21,16 @@ class EndByTimeShould {
         assertThat(actual).isEqualTo(ONE);
     }
 
+    @Test
+    public void determine_no_winner_when_time_is_over_and_goal_limit_was_not_reached_but_both_teams_has_an_equal_score() {
+        Stack<Team> overview = new Stack<>();
+        overview.push(ONE);
+        overview.push(TWO);
+        EndByTime endByTime = new EndByTime(overview);
+
+        Team actual = endByTime.getMatchWinner();
+
+        assertThat(actual).isEqualTo(NO_TEAM);
+
+    }
 }
