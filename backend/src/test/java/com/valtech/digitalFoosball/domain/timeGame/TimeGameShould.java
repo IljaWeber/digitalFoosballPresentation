085e2a7daf23073d5cf5a39a218ca293b45e8504
Ilja Weber <ilja.weber@valtech.com>
2020-08-03 11:06:43 +0200

    time game shows a won game which has won by reaching the score limit of 10, with team and player names

--- backend/src/test/java/com/valtech/digitalFoosball/domain/timeGame/TimeGameShould.java
+++ backend/src/test/java/com/valtech/digitalFoosball/domain/timeGame/TimeGameShould.java
@@ -20,28 +20,26 @@ class TimeGameShould {
     @BeforeEach
     void setUp() {
         timeGame = new TimeGame(new AdHocInitService());
+        timeGame.initGame(new InitDataModel());
     }
 
     @Test
     public void start_a_time_game() {
         timeGame.initGame(new InitDataModel());
 
-        timeGame.countGoalFor(ONE);
         GameOutputModel outputModel = timeGame.getGameData();
         List<TeamOutputModel> actual = outputModel.getTeams();
         assertThat(actual).extracting(TeamOutputModel::getName).containsExactly("Orange", "Green");
         assertThat(actual).extracting(TeamOutputModel::getPlayerOne).containsExactly("Goalie", "Goalie");
         assertThat(actual).extracting(TeamOutputModel::getPlayerTwo).containsExactly("Striker", "Striker");
-        assertThat(actual).extracting(TeamOutputModel::getScore).containsExactly(1, 0);
+        assertThat(actual).extracting(TeamOutputModel::getScore).containsExactly(0, 0);
         assertThat(outputModel.getMatchWinner()).isEqualTo(NO_TEAM);
     }
 
     @Test
-    public void prepare_a_game_in_first_half_with_team_and_player_names_with_their_scores() {
-        timeGame.initGame(new InitDataModel());
-
-        raiseScore(ONE, ONE,
-                   TWO);
+    public void prepare_a_game_in_first_half_with_team_and_player_names_with_their_scores_for_output() {
+        raiseScoreFor(ONE, ONE,
+                      TWO);
 
         TimeGameOutputModel gameData = timeGame.getGameData();
         List<TeamOutputModel> actual = gameData.getTeams();
@@ -53,7 +51,25 @@ class TimeGameShould {
         assertThat(gameData.getMatchWinner()).isEqualTo(NO_TEAM);
     }
 
-    private void raiseScore(Team... teams) {
+    @Test
+    public void show_game_which_has_won_by_reaching_the_score_limit_with_the_winner_all_team_and_player_names_and_their_scores() {
+        raiseScoreFor(ONE, ONE,
+                      ONE, ONE,
+                      ONE, ONE,
+                      ONE, ONE,
+                      ONE, ONE);
+
+        TimeGameOutputModel gameData = timeGame.getGameData();
+        List<TeamOutputModel> actual = gameData.getTeams();
+        assertThat(actual).extracting(TeamOutputModel::getName).containsExactly("Orange", "Green");
+        assertThat(actual).extracting(TeamOutputModel::getPlayerOne).containsExactly("Goalie", "Goalie");
+        assertThat(actual).extracting(TeamOutputModel::getPlayerTwo).containsExactly("Striker", "Striker");
+        assertThat(actual).extracting(TeamOutputModel::getScore).containsExactly(10, 0);
+        assertThat(gameData.getGameState()).isEqualTo(GameState.OVER_BY_SCORE);
+        assertThat(gameData.getMatchWinner()).isEqualTo(ONE);
+    }
+
+    private void raiseScoreFor(Team... teams) {
         for (Team team : teams) {
             timeGame.countGoalFor(team);
         }
