e928176bed478d10a89a9102ed300441ee8ce837
m.huber <maximilian.huber@valtech.com>
2020-08-20 11:12:51 +0200

    prepared frontend for real use

--- frontend/src/main/resources/properties.js
+++ frontend/src/main/resources/properties.js
@@ -1,15 +1,14 @@
-/*
 export const properties = {
     url: "https://digital-football.valtech.io/api/",
     hostAndPort: 'https://digital-football.valtech.io/',
     auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
 };
-*/
 
+/*
 export const properties = {
     userCommandsUrl: "http://localhost:8888/",
     hostAndPort: 'http://localhost:8888/',
     sock_url: "ws://localhost:8888/",
     auth: 'Basic ZGlnaXRhbC5mb290YmFsbDpVUnlnIyM0fSMpSzpcXHIyflErS301KSM/NURXM0cnXiQ9b3I6OyY8K31gJ1dVP01QeA=='
 };
-
+*/
